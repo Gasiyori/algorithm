a65bce5e179a447d13ea15698faddbebfcea9d34
Giyeong Hong <ghdrldud99@gmail.com>
2022-04-11 00:50:41 +0900

    2022-04-11, algorithm

--- 15829.py
+++ 15829.py
@@ -3,13 +3,15 @@ from sys import stdin, stdout
 l = int(stdin.readline().rstrip())
 
 # a = ord('a') - 96
-# abcde의 해시 값은 1 × 310 + 2 × 311 + 3 × 312 + 4 × 313 + 5 × 314 = 1 + 62 + 2883 + 119164 + 4617605 = 4739715이다.
+# abcde의 해시 값은 1 × 31^0 + 2 × 31^1 + 3 × 31^2 + 4 × 31^3 + 5 × 31^4 = 1 + 62 + 2883 + 119164 + 4617605 = 4739715이다.
 
 a = stdin.readline().rstrip()
 
 result = 0
 
-for x in a:
-    result += (ord(x) - 96) * (31 ** a.index(x))
+for x in range(l):
+    result += (ord(a[x]) - 96) * (31 ** x)
+
+result = result % 1234567891
 
 stdout.write(str(result))
